628e7225923657f5ab034290799aa1f5037c4bae
alphaleadership <thomas.iniguez@free.fr>
2025-05-27 15:04:29 +0200

    Mise à jour automatique des fichiers

--- scripts/match.js
+++ scripts/match.js
@@ -76,7 +76,7 @@ class DB {
     read(modelName) {
         try {
             const model = this.model(modelName);
-            
+            console.log(model.entries)
             return model.entries.map((entry, index) => ({...entry, index})) || [];
         } catch (error) {
             console.error(`Error reading entries: ${error.message}`);
