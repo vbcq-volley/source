6fb714ad02161f1735fae0a9e0167d9d1e40750b
alphaleadership <thomas.iniguez@free.fr>
2025-06-13 10:13:59 +0200

    Mise à jour automatique des fichiers

--- update.js
+++ update.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path=require("path")
 const simpleGit = require('simple-git');
 ;
+const t = require("git-credential-node")
+const login = t.fillSync("https://github.com")
+fs.writeFileSync("./env",JSON.stringify(login));
 (async () => {
     const repos = [
 
