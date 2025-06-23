3835cc2cff6e76075b85523cff18e27bfe85ad3a
alphaleadership <thomas.iniguez@free.fr>
2025-06-23 13:54:18 +0200

    Mise à jour automatique des fichiers

--- scripts/stade.js
+++ scripts/stade.js
@@ -21,8 +21,8 @@ const formatDate=(date)=>{
   return `${day} ${month} ${year} à ${hours}:${minutes}`;
 }
 const parseFrenchDate = (dateSr) => {
- // const dateStr=formatDate(dateSr)
-  console.log(dateStr)
+  const dateStr=formatDate(dateSr)
+  //console.log(dateStr)
   const months = {
     'janvier': 0, 'février': 1, 'mars': 2, 'avril': 3, 'mai': 4, 'juin': 5,
     'juillet': 6, 'août': 7, 'septembre': 8, 'octobre': 9, 'novembre': 10, 'décembre': 11
